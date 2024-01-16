c3dee5f0e666bb068858602899c3cfc6de030a23
Mohammed AL-Harbi <mojazi42@gmail.com>
2024-01-16 07:23:21 +0300

    'index.html'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,39 +1,33 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+  angular.module('LunchCheck', [])
+      .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.checkDishes = function () {
-    var num = countDishes($scope.dishes);
-    $scope.message = buildMessage(num);
-  };
+  LunchCheckController.$inject = ['$scope'];
 
-  function countDishes(dishes) {
-    var count = 0;
-    if (dishes) {
-      var array = dishes.split(',');
-      for (var idx in array) {
-        if (array[idx].trim().length != 0) {
-          count++;
-        }
-      }
-    }
-    return count;
+  function LunchCheckController($scope) {
+      $scope.dishes = '';
+      $scope.message = '';
+      $scope.checked = false;
+
+      $scope.checkLunch = function() {
+          if ($scope.dishes.trim().length === 0) {
+              $scope.empty = true;
+          } else {
+              $scope.checked = true;
+              $scope.empty = false;
+              var arrayDishes = $scope.dishes.split(',');
+              var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                  return v.trim() !== '';
+              });
+
+              if (arrayDishesWithoutEmptys.length <= 3) {
+                  $scope.message = 'Enjoy!';
+              } else {
+                  $scope.message = 'Too much!';
+              }
+          }
+      };
   }
-
-  function buildMessage(num) {
-    if (num === 0) {
-      return 'Please enter data first';
-    }
-    else if (num <= 3) {
-      return 'Enjoy!';
-    } else {
-      return 'Too much!';
-    }
-  }
-}
-
 })();
